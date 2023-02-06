79b66e461299e13a10d0f06a7b7d4d9f8756804b
Luca Morelli <luca.morelli.lm@gmail.com>
2023-02-06 10:11:53 +0100

    style: move Personinfo style to index.css

--- src/PersonInfo.tsx
+++ src/PersonInfo.tsx
@@ -12,17 +12,6 @@ function PersonInfo(props: Props) {
   const { data } = props;
   return (
     <div
-      style={{
-        display: "flex",
-        height: "100px",
-        justifyContent: "center",
-        flexDirection: "column",
-        padding: "32px",
-        boxShadow: "0px 1px 2px 0px rgba(0, 0, 0, 0.15)",
-        margin: "10px 0",
-        background: "#fff",
-        cursor: "pointer",
-      }}
       className="person-info"
     >
       <div className="firstNameLastName">{data.firstNameLastName}</div>
